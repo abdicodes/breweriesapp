3c6255d0326d3aa95faab68a1e5709eeaadd3323
Abdirahman Suldaan <suldaan.89@hotmail.com>
2022-04-28 16:04:47 +0300

    init: importing required libraries.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+// import { Link } from 'react-router-dom'
+import { Grid, Container, CssBaseline } from '@mui/material/'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <CssBaseline />
+      <Container>
+        <Grid container spacing={3}></Grid>
+      </Container>
+    </>
+  )
 }
 
-export default App;
+export default App
